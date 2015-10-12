efbc3fe5981ffb1fd79cade351a1fe8386bd498a
Sylvain Gourio <sylvain.gourio@gmail.com>
2015-10-12 19:43:00 +0200

    font awesome font

--- gulp/build.js
+++ gulp/build.js
@@ -48,6 +48,7 @@ module.exports = function(options) {
       .pipe(jsFilter.restore())
       .pipe(cssFilter)
       .pipe($.replace('../../bower_components/bootstrap-sass-official/assets/fonts/bootstrap/', '../fonts/'))
+      .pipe($.replace('../../bower_components/font-awesome/fonts', '../fonts/'))
       .pipe($.csso())
       .pipe(cssFilter.restore())
       .pipe(assets.restore())
